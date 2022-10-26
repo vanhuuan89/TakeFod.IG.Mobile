11a3ad5c49b6e4efe215df927fbbb1ec4f4202ed
vanhuuan89 <andeptrai89@gmail.com>
2022-10-26 14:34:20 +0700

    init ocelot

--- TakeFood.IG.Mobile/Program.cs
+++ TakeFood.IG.Mobile/Program.cs
@@ -1,11 +1,18 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+IConfiguration configuration = new ConfigurationBuilder()
+                                       .AddJsonFile("ocelot.json")
+                                       .Build();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddOcelot(configuration);
 
 var app = builder.Build();
 
@@ -17,6 +24,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseOcelot();
 
 app.UseAuthorization();
 

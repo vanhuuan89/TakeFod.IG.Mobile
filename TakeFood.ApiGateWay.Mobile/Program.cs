2775f7b4894b8e5123ab7379db55d5c9ddac1e3a
Văn Hữu An <58378623+vanhuuan89@users.noreply.github.com>
2022-10-31 21:11:29 +0700

    add authorization

--- TakeFood.ApiGateWay.Mobile/Program.cs
+++ TakeFood.ApiGateWay.Mobile/Program.cs
@@ -1,13 +1,30 @@
+using Microsoft.IdentityModel.Tokens;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
     .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true)
     .AddEnvironmentVariables();
+builder.Configuration.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+    .AddEnvironmentVariables();
 builder.Services.AddOcelot(builder.Configuration);
 builder.Services.AddSwaggerForOcelot(builder.Configuration);
 builder.Services.AddControllers();
+string key = builder.Configuration.GetSection("JwtConfig:Secret").Value;
+
+builder.Services.AddAuthentication().AddJwtBearer("Bearer", options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key)),
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ValidateLifetime = true
+    };
+});
 var app = builder.Build();
 app.UseSwaggerForOcelotUI(opt =>
 {
